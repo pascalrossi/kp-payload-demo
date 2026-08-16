6a6e7532d2eeadfd8f2e18e76053c075f93f134e
Pascal Rossi <pascal@alignz.com>
2026-08-16 11:59:01 +0200

    Remove configPath from withPayload (not supported in 3.88)

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,8 @@
 import { withPayload } from '@payloadcms/next/withPayload'
 import type { NextConfig } from 'next'
-import path from 'path'
 
 const nextConfig: NextConfig = {
   output: 'standalone',
 }
 
-export default withPayload(nextConfig, {
-  configPath: path.resolve('./src/payload.config.ts'),
-})
+export default withPayload(nextConfig)
